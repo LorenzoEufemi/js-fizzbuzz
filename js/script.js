34f498dfe827362b86f54f02d1126aea0bacee2c
LorenzoEufemi <lorenzoeufemi@gmail.com>
2024-10-29 16:05:42 +0100

    feat: finalizzazione FizzBuzz

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
+// esecuzione
+for (let num = 1 ; num <= 100 ; num++) {
+    
+    if ( num % 3 === 0 &&  num % 5 === 0) {
+        console.log("FizzBuzz");
+    } else if ( num % 3 === 0) {
+        console.log("Fizz");
+    } else if ( num % 5 === 0) {
+        console.log("Buzz");
+    } else {
+        console.log(num)
+    }
+}
 
-for (let i = 1 ; i <= 100 ; i++);
-console.log(i)
     
